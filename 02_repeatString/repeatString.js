7b67f7b7c78325ef9370e1838c6f9108f9c6ebe4
rakesh-i <rakesh.007.ac@gmail.com>
2023-08-18 22:07:46 +0530

    1 2 and three are complete

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    let a = "";
+    if(string ==""){
+        return "";
+    }
+    if(num<0){
+        return "ERROR";
+    }
+    for(let i=0; i<num; i++){
+        a += string;
+    }
+    return a;
 };
 
 // Do not edit below this line
--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let n = string.length;
+    let a = "";
+    for(let i=n-1; i>=0; i--){
+        a += string[i];
+    }
+    return a;
 };
 
 // Do not edit below this line

5ce2bc9ab19895ad1d1cd9b02ab7296ebd369042
rakesh-i <rakesh.007.ac@gmail.com>
2023-08-19 21:37:04 +0530

    09 complete

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,18 @@
-const fibonacci = function() {
-
+const fibonacci = function(a) {
+    if(a<0){
+        return 'OOPS';
+    }
+    if(a<3){
+        return 1;
+    }
+    let x1 = 1;
+    let x2 = 1;
+    for(let i=2; i<a; i++){
+        let s = x1+x2;
+        x1 = x2;
+        x2 = s;
+    }
+    return x2;
 };
 
 // Do not edit below this line

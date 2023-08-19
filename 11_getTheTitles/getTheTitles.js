15628c5f93aea1d9e076cc5499b5933ede24d7c2
rakesh-i <rakesh.007.ac@gmail.com>
2023-08-19 21:45:38 +0530

    11 complete

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,9 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(a) {
+    const x = [];
+    for(let i=0; i<a.length; i++){
+        x[i] = a[i].title;
+    }
+    return x;
 };
 
 // Do not edit below this line

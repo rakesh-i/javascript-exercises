881ff24cd09ef4b7941fa9485a50b3e07272de74
rakesh-i <rakesh.007.ac@gmail.com>
2023-08-19 21:00:38 +0530

    08 complete

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,20 @@
-const palindromes = function () {
-
+const palindromes = function (s) {
+    let x = s.toUpperCase();
+    let y = "";
+    for(let i=0; i<x.length; i++){
+        if(x.charCodeAt(i)<91&&x.charCodeAt(i)>64||x.charCodeAt(i)<58&&x.charCodeAt(i)>47){
+            y +=x[i];
+        }
+    }
+    //return y;
+    let z = "";
+    for(let i=y.length-1; i>=0; i--){
+        z+=y[i];
+    }
+    if(z==y){
+        return true;
+    }
+    return false;
 };
 
 // Do not edit below this line

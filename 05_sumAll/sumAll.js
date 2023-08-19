0ef6fefee15aeb1873d1a0987b273fbb2c6ffde1
rakesh-i <rakesh.007.ac@gmail.com>
2023-08-19 19:51:58 +0530

    05 complete

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
-
+const sumAll = function(a, b) {
+    if(typeof a != 'number'||typeof b!= "number"){
+        return 'ERROR';
+    }
+    let l = (a>b)?a:b;
+    let s = (a>b)?b:a;
+    let sub = 0;
+    if(s<0){
+        // s = Math.abs(s);
+        // sub = s*(s+1)/2;
+        return 'ERROR';
+    }
+    let sum = l*(l+1)/2;
+    return sum-sub;
 };
 
 // Do not edit below this line

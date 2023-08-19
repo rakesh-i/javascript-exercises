bf5b1086e555be3ca82a8605c396f69571f832d6
rakesh-i <rakesh.007.ac@gmail.com>
2023-08-19 20:21:13 +0530

    07 complete

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,12 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+  const c = 5*(temp-32)/9;
+  
+  return (c%2==0)?c:Number(c.toFixed(1));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  const f = 9/5*temp+32;
+  return (f%2==0)?f:Number(f.toFixed(1));
 };
 
 // Do not edit below this line

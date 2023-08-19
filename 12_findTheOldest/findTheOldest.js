3bd686a7f5986356787883d78c81fd2a130f101a
rakesh-i <rakesh.007.ac@gmail.com>
2023-08-19 22:17:38 +0530

    all complete

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,22 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    let x = 0;
+    let age = 0;
+    let n = people.length;
+    for(let i=0; i<n; i++){
+        const curr = getage(people[i].yearOfBirth, people[i].yearOfDeath);
+        if(age<curr){
+            age = curr;
+            x= i;
+        }
+    }
+    return people[x];
+};
 
+const getage = function(birth, death){
+    if(!death){
+        death = new Date().getFullYear();
+    }
+    return death - birth;
 };
 
 // Do not edit below this line
